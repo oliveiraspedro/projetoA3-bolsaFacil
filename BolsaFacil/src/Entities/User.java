378a36e767d0752a583e4f9e69c91c9420022b9b
oliveiraspedro <pedro.santosdeoliveira13@gmail.com>
2024-11-13 13:35:47 -0300

    Removido arquivos/pastas antigos do repositorio

--- BolsaFacil/src/Entities/User.java
+++
@@ -1,9 +0,0 @@
-package Entities;
-
-public interface User {
-    public String getEmail();
-    public void setEmail();
-    public void setSenha();
-    public boolean login();
-    public boolean logout();
-}
--- BolsaFacil/src/Enums/UserTypes.java
+++
@@ -1,7 +0,0 @@
-package Enums;
-
-public enum UserTypes {
-    TYPE_ALUNO,
-    TYPE_ADMIN,
-    TYPE_INST
-}
